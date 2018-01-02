a4e96eda4606a3f8ff2c9d493816904d54c0e702
Vaibhav Gharad <vaibhav.gharad@pb.com>
2018-01-02 22:53:53 +0530

    code commit for the UI

--- src/main/java/com/reviews/app/models/OutputTableView.java
+++ src/main/java/com/reviews/app/models/OutputTableView.java
@@ -8,9 +8,16 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
-import com.reviews.app.service.ReviewService;
-
 import lombok.Data;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+
+import com.reviews.app.service.ReviewService;
 
 @ManagedBean(name = "outputTableView")
 @ViewScoped
@@ -41,4 +48,20 @@ public class OutputTableView implements Serializable {
 		reviewService.updateOutputTableEntry(selectedOutputTableEntry);
 		return null;
 	}
+	
+	 public void postProcessXLS(Object document) {
+	        HSSFWorkbook wb = (HSSFWorkbook) document;
+	        HSSFSheet sheet = wb.getSheetAt(0);
+	        HSSFRow header = sheet.getRow(0);
+	         
+	        HSSFCellStyle cellStyle = wb.createCellStyle();  
+	        cellStyle.setFillForegroundColor(HSSFColor.GREEN.index);
+	        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+	         
+	        for(int i=0; i < header.getPhysicalNumberOfCells();i++) {
+	            HSSFCell cell = header.getCell(i);
+	             
+	            cell.setCellStyle(cellStyle);
+	        }
+	    }
 }
