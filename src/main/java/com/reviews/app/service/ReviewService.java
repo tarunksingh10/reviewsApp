5f9e83ecd306e6968b43d325ce49cf0375d8ea90
Tarun Singh <tarun.singh@pb.com>
2018-02-10 11:01:43 +0530

    Adding auth header for UI

--- src/main/java/com/reviews/app/service/ReviewService.java
+++ src/main/java/com/reviews/app/service/ReviewService.java
@@ -6,14 +6,17 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.reviews.app.models.OutputTable;
+
+import lombok.extern.slf4j.Slf4j;
 
 @ManagedBean(name = "reviewService")
 @ViewScoped
@@ -33,8 +36,12 @@ public class ReviewService {
 		try {
 			HttpHeaders headers = new HttpHeaders();
 			log.info("Method Name is  :" + METHOD_NAME + " url :" + "http://nipsilon.us-east-2.elasticbeanstalk.com");
-			OutputTable[] response =  rest.getForObject("http://nipsilon.us-east-2.elasticbeanstalk.com/review/outputTableEntry", OutputTable[].class);
-			getOutputTableList = Arrays.asList(response);
+			headers.add("Authorization", "Basic bWFudGhhbjpHKlpLQWd7OHZUY0h5N1Jz");
+			HttpEntity<?> httpentity = new HttpEntity<>(headers);
+			ResponseEntity<OutputTable[]> apiResponse = rest.exchange("http://nipsilon.us-east-2.elasticbeanstalk.com/review/outputTableEntry",
+					HttpMethod.GET, httpentity, OutputTable[].class);
+			OutputTable[] responsebody = apiResponse.getBody();
+			getOutputTableList = Arrays.asList(responsebody);
 		} catch (Exception e) {
 			log.error("Method Name :" + METHOD_NAME + " Unable to fetch  details. Error: " + e.getMessage());
 			e.printStackTrace();
@@ -42,19 +49,23 @@ public class ReviewService {
 		log.info("Exit " + METHOD_NAME);
 		return getOutputTableList;
 	}
-	
-	
-	public void updateOutputTableEntry(OutputTable selectedOutputTableEntry){
+
+	public void updateOutputTableEntry(OutputTable selectedOutputTableEntry) {
 		final String METHOD_NAME = "updateOutputTableEntry";
 		log.info("ENTRY " + METHOD_NAME);
 		try {
-			log.info("Method Name :" + METHOD_NAME + " url :" + "http://nipsilon.us-east-2.elasticbeanstalk.com/review/outputTableEntry");
-			 rest.put("http://nipsilon.us-east-2.elasticbeanstalk.com/review/outputTableEntry/"+selectedOutputTableEntry.getId(), selectedOutputTableEntry);
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Authorization", "Basic bWFudGhhbjpHKlpLQWd7OHZUY0h5N1Jz");
+			HttpEntity<OutputTable> httpentity = new HttpEntity<>(selectedOutputTableEntry, headers);
+			log.info("Method Name :" + METHOD_NAME + " url :"
+					+ "http://nipsilon.us-east-2.elasticbeanstalk.com/review/outputTableEntry");
+			rest.put("http://nipsilon.us-east-2.elasticbeanstalk.com/review/outputTableEntry/"
+					+ selectedOutputTableEntry.getId(), httpentity);
 		} catch (Exception e) {
 			log.error("Method Name :" + METHOD_NAME + " Unable to update  details. Error: " + e.getMessage());
 			e.printStackTrace();
 		}
 		log.info("Exit " + METHOD_NAME);
-		
+
 	}
 }
